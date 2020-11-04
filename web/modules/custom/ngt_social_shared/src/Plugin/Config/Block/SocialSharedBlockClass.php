bd5b3023bf355642e8c901cb449ee7336ee4be14
David Martinez <davidmartinezb1@hotmail.com>
2020-11-04 00:02:33 -0500

    ajuste modulo compartir

--- web/modules/custom/ngt_social_shared/src/Plugin/Config/Block/SocialSharedBlockClass.php
+++ web/modules/custom/ngt_social_shared/src/Plugin/Config/Block/SocialSharedBlockClass.php
@@ -46,12 +46,17 @@ class SocialSharedBlockClass {
         $url_twitter  =  $config->get('ngt_social_shared_config')['twitter_url'] .'?url='. $url .'&via='. $via .'&hashtags='. $twitter_hashtags;
         $url_facebook  =  $config->get('ngt_social_shared_config')['facebook_url'] .'?u='. $url;
         
+        $activate_facebook = isset($config->get('ngt_social_shared_active')['facebook']) ? $config->get('ngt_social_shared_active')['facebook'] : 0;
+        $activate_twitter = isset($config->get('ngt_social_shared_active')['twitter']) ? $config->get('ngt_social_shared_active')['twitter'] : 0;
+
         $build = [
             '#theme' => 'social_shared_button',
             '#url' => $url,
             '#via' => $via,
             '#url_facebook' => $url_facebook,
             '#url_twitter' => $url_twitter,
+            '#active_facebook' => $activate_facebook,
+            '#active_twitter' => $activate_twitter,
         ];
 
         return $build;
